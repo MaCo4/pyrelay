9e58bf459f8e754c30805f375bec15d11856ca19
Magnus Conrad Hyll <magnus@hyll.no>
2017-09-08 09:35:27 +0200

    First code commit

---
+++ src/relay_host.py
@@ -0,0 +1,39 @@
+import socket
+import threading
+
+
+relay_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+relay_server.bind(("", 9877))
+relay_server.listen(1)
+
+relay_server_conn, relay_server_addr = relay_server.accept()
+print("Recieved connection from relayed server at {}".format(relay_server_addr))
+
+relay_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+relay_client.bind(("", 9878))
+relay_client.listen(1)
+
+
+def serve_connection(client_conn):
+    request = client_conn.recv(1000000)
+    print("Request from client: {}".format(request.split(b"\r\n")[0]))
+
+    relay_server_conn.send(request)
+    response = relay_server_conn.recv(1000000)
+    print("Response from relayed server: {}".format(response.split(b"\r\n")[0]))
+
+    client_conn.send(response)
+    client_conn.close()
+
+
+connection_threads = []
+while True:
+    client_conn, client_addr = relay_client.accept()
+    print("Recieved connection from client at {}".format(client_addr))
+    thread = threading.Thread(target=serve_connection, args=(client_conn, ))
+    connection_threads.append(thread)
+    thread.start()
+
+
+relay_client.close()
+relay_server.close()
---
+++ src/relay_server.py
@@ -0,0 +1,20 @@
+import socket
+
+
+relay_host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+relay_host.connect(("tihlde.org", 9877))
+while True:
+    request = relay_host.recv(1000000)
+    print("Request from relay host: {}".format(request.split(b"\r\n")[0]))
+
+    local_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    local_server.connect(("127.0.0.1", 80))
+
+    local_server.send(request)
+    response = local_server.recv(1000000)
+    print("Response from local server: {}".format(response.split(b"\r\n")[0]))
+
+    relay_host.send(response)
+    local_server.close()
+
+relay_host.close()
