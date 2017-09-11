014b7c73f2f722cbab89e0ac462d5c742e765670
Magnus Conrad Hyll <magnus@hyll.no>
2017-09-11 21:54:44 +0200

    WIP connection multiplexing and buffering

--- src/relay_host.py
+++ src/relay_host.py
@@ -1,25 +1,21 @@
+import queue
 import socket
 import threading
 
 
-relay_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-relay_server.bind(("", 9877))
-relay_server.listen(1)
-
-relay_server_conn, relay_server_addr = relay_server.accept()
-print("Recieved connection from relayed server at {}".format(relay_server_addr))
-
-relay_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-relay_client.bind(("", 9878))
-relay_client.listen(1)
-
-
-def serve_connection(client_conn):
+def serve_client_conn(client_conn: socket.socket):
     while True:
-        request = client_conn.recv(4096)
+        request = b""
+        while True:
+            request_part = client_conn.recv(4096)
+            if not request_part:
+                break
+            request += request_part
+
         print("Request from client: {}".format(request.split(b"\r\n")[0]))
 
-        relay_server_conn.send(request)
+        send_to_server(client_conn, request)
+
         response = relay_server_conn.recv(4096)
         print("Response from relayed server: {}".format(response.split(b"\r\n")[0]))
 
@@ -28,14 +24,51 @@ def serve_connection(client_conn):
     client_conn.close()
 
 
+def send_to_server(source, data):
+    if len(data) > 0xffff:
+        return
+
+    source_addr = source.getpeername()
+
+    message = b"\x02"                                             # Packet type (0x02 for normal data), 1 byte
+    message += socket.inet_aton(source_addr[0])                   # Source IP address, 4 bytes
+    message += source_addr[1].to_bytes(2, "big")                  # Source port number, 2 bytes
+    message += (b"\x00" * 2 + len(data).to_bytes(2, "big"))[-2:]  # Payload length, 2 bytes
+    message += data                                               # Payload
+
+    server_message_queue.put(message)
+
+
+def process_send_queue(server_connection: socket.socket, server_message_queue: queue.Queue):
+    while True:
+        try:
+            message = server_message_queue.get(timeout=10)
+            server_connection.send(message)
+        except queue.Empty:
+            pass
+            # TODO Send keep-alive pakke hvis ingenting annet er sendt på en stund
+
+
+relay_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+relay_server.bind(("", 9877))
+relay_server.listen(1)
+
+relay_server_conn, relay_server_addr = relay_server.accept()
+server_message_queue = queue.Queue()
+print("Received connection from endpoint server at {}".format(relay_server_addr))
+threading.Thread(target=process_send_queue, args=(relay_server_conn, server_message_queue)).start()
+
+relay_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+relay_client.bind(("", 9878))
+relay_client.listen(1)
+
 connection_threads = []
 while True:
     client_conn, client_addr = relay_client.accept()
     print("Recieved connection from client at {}".format(client_addr))
-    thread = threading.Thread(target=serve_connection, args=(client_conn, ))
+    thread = threading.Thread(target=serve_client_conn, args=(client_conn,))
     connection_threads.append(thread)
     thread.start()
-
 
 relay_client.close()
 relay_server.close()
--- src/relay_server.py
+++ src/relay_server.py
@@ -1,20 +1,91 @@
 import socket
+import queue
+import threading
+
+
+class Request:
+    ip_addr = None
+    port = None
+    payload = None
+
+    def __init__(self, ip_addr=None, port=None, payload=None):
+        self.ip_addr = ip_addr
+        self.port = port
+        self.payload = payload
+
+
+def read_relay_socket(relay_host: socket.socket, request_queue: queue.Queue):
+    buffer = b""
+    while True:
+        buffer += relay_host.recv(9)
+        if buffer == b"":
+            break  # TODO Dette betyr at tilkoblingen er brutt
+
+        while len(buffer) < 9:
+            buffer += relay_host.recv(9)
+
+        header = buffer[:9]
+        buffer = buffer[9:]  # Fjern header-delen fra bufferet, den er behandlet
+
+        if header[0] == 0x02:
+            request = Request()
+            request.ip_addr = header[1:5]
+            request.port = header[5:7]
+            payload_len = header[7:9]
+
+            buffer += relay_host.recv(payload_len)
+            while len(buffer) < payload_len:
+                buffer += relay_host.recv(payload_len)
+
+            request.payload = buffer[:payload_len]
+            buffer = buffer[payload_len:]  # Fjern data i bufferet som vi har behandlet
+
+            request_queue.put(request)
+
+        elif header[0] == 0x01:
+            # TODO Svar på keepalive?
+            pass
+
+        else:
+            print("Unknown packet type: {}".format(buffer[0]))
 
 
 relay_host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 relay_host.connect(("tihlde.org", 9877))
+
+request_queue = queue.Queue()
+threading.Thread(target=read_relay_socket, args=(relay_host, request_queue)).start()
+
 while True:
-    request = relay_host.recv(4096)
-    print("Request from relay host: {}".format(request.split(b"\r\n")[0]))
+    request = request_queue.get()
+
+    print("Request from relay host: {}".format(request.payload.split(b"\r\n")[0]))
 
     local_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     local_server.connect(("127.0.0.1", 80))
+    local_server.send(request.payload)
 
-    local_server.send(request)
     response = local_server.recv(4096)
+    while True:
+        response_part = local_server.recv(4096)
+        if response_part == b"":
+            break
+
+        response += response_part
+
+    local_server.close()
+
     print("Response from local server: {}".format(response.split(b"\r\n")[0]))
 
-    relay_host.send(response)
-    local_server.close()
+    while len(response) > 0:
+        part_response = response[:0xffff]
+        response_message = b"\x02"
+        response_message += request.ip_addr
+        response_message += request.port
+        response_message += (b"\x00" * 2 + len(part_response).to_bytes(2, 'big'))[-2:]
+        response_message += part_response
+
+        relay_host.send(response_message)
+        response = response[0xffff:]
 
 relay_host.close()
